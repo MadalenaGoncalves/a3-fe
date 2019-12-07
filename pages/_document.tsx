ffb20390d62bc8e3ea4f42f1b6572b1676377e97
madpg <madalena.pg@gmail.com>
2019-12-07 13:19:16 +0100

    update favicon path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-          <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/static/images/favicon.ico" />
         </Head>
         <body>
           <Main />
