b9481b1b2905788262334bdfef409ecceac54a13
madpg <madalena.pg@gmail.com>
2019-12-08 15:35:54 +0100

    Added MUI ServerStyleSheets for ssr and updated package-lock

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,32 @@
 import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet as StyledServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets as MuiServerStyleSheet } from '@material-ui/core/styles';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const styledSheet = new StyledServerStyleSheet();
+    const muiSheet = new MuiServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => styledSheet.collectStyles(muiSheet.collect(<App {...props} />)),
         })
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
+            {styledSheet.getStyleElement()}
+            {muiSheet.getStyleElement()}
           </>
         ),
       }
     } finally {
-      sheet.seal()
+      styledSheet.seal();
     }
   }
 
