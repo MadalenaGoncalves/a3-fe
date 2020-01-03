aad2cb3cdea69c23235cc8f31a57d31c4ad6a285
madpg <madalena.pg@gmail.com>
2020-01-03 12:39:51 +0100

    Delete unused components

--- components/PageLayout/PageTitle.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-import { font__pageTitle } from '../../styles/styleguide/typography';
-
-export default styled.h1`
-  ${font__pageTitle};
-  text-align: center;
-`;
