fe2f420b9f073ce68e7a55c285857de88874ef6b
madpg <madalena.pg@gmail.com>
2020-01-03 11:26:19 +0100

    Ensure styled-components styles are injected after mui-styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import App from 'next/app'
 import * as React from 'react'
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components'
+import { StylesProvider } from '@material-ui/core/styles';
 
 import GlobalStyles from '../styles/global-styles';
 import theme from '../styles/theme';
@@ -13,12 +14,14 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
     return (
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <React.Fragment>
-            <GlobalStyles />
-            <Component {...pageProps} />
-          </React.Fragment>
-        </ThemeProvider>
+        <StylesProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <>
+              <GlobalStyles />
+              <Component {...pageProps} />
+            </>
+          </ThemeProvider>
+        </StylesProvider>
       </Provider>
     )
   }
